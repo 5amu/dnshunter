1991fbc32251f8fead542e1e76c84de547961997
5amu <casalinovalerio.cv@gmail.com>
2022-07-06 10:44:06 +0200

    DNS amplification check (ANY queries) implemented

--- internal/common/constants.go
+++ internal/common/constants.go
@@ -1,4 +1,9 @@
 package common
 
-const DNSHunterVersion = "0.1"
-const DefaultNameserver = "8.8.8.8"
+const (
+	DNSHunterVersion  = "0.1"
+	DefaultNameserver = "8.8.8.8"
+	// DNSAmplificationThreshold is an arbitrary number that the programmer
+	// considered to be enough for "response considerably larger than request"
+	DNSAmplificationThreshold = 5
+)
